ee89bfe0abacb143dc6af01b39f4911ae41e1b67
Sashini Ruwanthika Weliketiya <sashiniruwanthika08@gmail.com>
2025-05-27 23:25:42 +0530

    Add files via upload

---
+++ DAY_04/Codes/vectors.m
@@ -0,0 +1,9 @@
+%create two vectors
+x = [0,1,2,3,4,5,6,7,8,9,10];
+y = [0,0.84,0.91,0.14,-0.76,-0.96,-0.86,-0.56,-0.58,-0.98,-0.25];
+
+figure;
+plot(x,y,'-O');
+title('Line Plot: y vs x');
+xlabel('x');
+ylabel('y');
