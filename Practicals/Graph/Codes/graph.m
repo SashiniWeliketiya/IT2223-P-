47856c9f93d8d88c364dbfbdb304347589beb50e
Sashini Ruwanthika Weliketiya <sashiniruwanthika08@gmail.com>
2025-06-05 16:43:50 +0530

    Update graph.m

--- Practicals/Graph/Codes/graph.m
+++ Practicals/Graph/Codes/graph.m
@@ -8,18 +8,3 @@ title('Graph');%add a title
 %G=graph([1 2], [2,3])
 plot(G);%plot the graph
 title('Graph');%add a title
-
-%directed
-G = graph([2 1], [2 4]); 
-plot(G);
-title('Directed Graph');
-
-
-
-%weighted
-s = [1 2 2]; %start nodes
-t = [2 3 4]; %end nodes
-weights = [10 5 7];
-G = graph(s, t,weights);
-plot(G, 'EdgeLabel',G.Edges.Weight);%plot the graph
-title('Weighted Undirected Graph');%add atitle
