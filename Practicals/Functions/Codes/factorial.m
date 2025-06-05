e76379bc5aae77f665457ceb7997577a5d659743
Sashini Ruwanthika Weliketiya <sashiniruwanthika08@gmail.com>
2025-06-05 16:34:17 +0530

    Add files via upload

---
+++ Practicals/Functions/Codes/factorial.m
@@ -0,0 +1,10 @@
+
+   n = input("Enter the number:");
+    fact = 1;
+
+    for i=1:n
+          fact = fact * i;
+    end
+ 
+   fprintf("Factorial is: %d\n",fact);
+ 
---
+++ Practicals/Functions/Codes/factorial_recursive.m
@@ -0,0 +1,13 @@
+
+n = input("Enter number: ");
+function f = fact_n(n)
+
+   if n==0||n==1
+    f=1;
+   else
+    f=n*fact_n(n-1);
+   end
+end
+
+fact = fact_n(n);
+fprintf("Factorial is: %d\n",fact);
